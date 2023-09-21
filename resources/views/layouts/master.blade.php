384dd379436ed74fc8ba7a3e3ecabc60b02bef29
nbena69 <naelbenaissapro@gmail.com>
2023-09-21 16:22:14 +0200

    RAS

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,44 +1,57 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="fr">
 <head>
-    <title>Les Employes</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    {!! Html::style('assets/css/bootstrap.css') !!}
-    {!! Html::style('assets/css/monStyle.css') !!}
-
-    {!! Html::script('assets/js/bootstrap.min.js') !!}
+    <title>GSB Frais</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/monStyle.css') }}">
+    <script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
     <!-- Fonts -->
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet'
-          type='text/css'>
+    <link href="//fonts.googleapis.com/css?family=Roboto:400,300" rel="stylesheet" type="text/css">
 </head>
 <body class="body">
+
 <div class="container">
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container-fluid">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" datatarget="#navbar-collapse-target">
-                    <span class="sr-only">Toggle navigation</span> <span class="iconbar"></span>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-target">
+                    <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
-                    <span class="icon-bar+ bvn"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ url('/') }}">Gestion des employés </a>
+                <a class="navbar-brand" href="{{ url('/') }}">GSB Frais</a>
             </div>
-            <div class="collapse navbar-collapse" id="navbar-collapse-target">
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/listerEmploye') }}" data-toggle="collapse" datatarget=".navbar-collapse.in">Lister</a></li>
-                    <li><a href="{{ url('/ajouterEmploye') }}" data-toggle"collapse" datatarget=".navbar-collapse.in">Ajouter </a></li>
-                </ul>
-            </div>
-        </div><!--/.container-fluid -->
+            @if (Session::get('id') == 0)
+                <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li><a href="" data-toggle="collapse" data-target=".navbar-collapse.in">Se connecter</a></li>
+                    </ul>
+                </div>
+            @endif
+            @if (Session::get('id') > 0)
+                <div class="collapse navbar-collapse" id="navbar-collapse-target">
+                    <ul class="nav navbar-nav">
+                        <li><a href="" data-toggle="collapse" data-target=".navbar-collapse.in">Lister</a></li>
+                        <li><a href="" data-toggle="collapse" data-target=".navbar-collapse.in">Ajouter</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="" data-toggle="collapse" data-target=".navbar-collapse.in">Se déconnecter</a></li>
+                    </ul>
+                </div>
+            @endif
+        </div>
     </nav>
 </div>
-</div>
+
 <div class="container">
     @yield('content')
 </div>
-{!! Html::script('assets/js/bootstrap.min.js') !!}
-{!! Html::script('assets/js/bootstrap.js') !!}
+
+<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $nael = 2;
     return view('home');
 });
