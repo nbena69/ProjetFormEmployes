7d59ca7a3f280ead75f61dfce2a44cb55148974d
nbena69 <naelbenaissapro@gmail.com>
2023-09-22 15:20:32 +0200

    Nouvel envoie

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use DipeshSukhia\LaravelHtmlMinify\Middleware\LaravelMinifyHtml;
 
 class Kernel extends HttpKernel
 {
@@ -21,6 +22,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        LaravelMinifyHtml::class
     ];
 
     /**
